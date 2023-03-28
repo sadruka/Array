e8647ff58b08138c71251c36cd0629ca6a54496c
sadruka <sadrukaafrin4@gmail.com>
2023-03-28 10:57:24 +0600

    main file add

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int array[10];
+    int i;
+
+    printf("input 10 elements in the array :\n");
+    for(i=0; i<10; i++){
+
+     printf(" element - %d : " ,i);
+     scanf("%d", & array [i]);
+
+    }
+
+    printf("\n elements in array are: ");
+    for(i=0; i<10; i++){
+        printf(" %d :", array[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
